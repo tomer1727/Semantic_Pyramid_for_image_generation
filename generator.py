6bfc37d3af1fc8464b59c48bf93fb5af29fe1700
tomer1727 <tomer1727@gmail.com>
2020-12-23 15:20:21 +0200

    add eval_model, continue implementations of generator

--- generator.py
+++ generator.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -40,6 +41,42 @@ class ResidualBlock(nn.Module):
         return x
 
 
+class SelfAttentionLayer(nn.Module):
+    """ Self attention Layer"""
+
+    def __init__(self, in_dim):
+        super(SelfAttentionLayer, self).__init__()
+        self.chanel_in = in_dim
+
+        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax = nn.Softmax(dim=-1)  #
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps( B X C X W X H)
+            returns :
+                out : self attention value + input feature
+                attention: B X N X N (N is Width*Height)
+        """
+        m_batchsize, C, width, height = x.shape
+        proj_query = self.query_conv(x).view(m_batchsize, -1, width * height).permute(0, 2, 1)  # B X CX(N)
+        proj_key = self.key_conv(x).view(m_batchsize, -1, width * height)  # B X C x (*W*H)
+        energy = torch.bmm(proj_query, proj_key)  # transpose check
+        attention = self.softmax(energy)  # BX (N) X (N)
+        proj_value = self.value_conv(x).view(m_batchsize, -1, width * height)  # B X C X N
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, width, height)
+
+        out = self.gamma * out + x
+        return out, attention
+
+
 class GeneratorBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
@@ -58,14 +95,18 @@ class Generator(nn.Module):
         self.gen_block1 = GeneratorBlock(2048, 1024)
         self.gen_block2 = GeneratorBlock(1024, 512)
         self.gen_block3 = GeneratorBlock(512, 256)
+        self.attn1 = SelfAttentionLayer(in_dim=256)
         self.gen_block4 = GeneratorBlock(256, 64)
         self.last_conv_block = ConvTransposeBlock(64, 3, 8, stride=2, padding=3)
+        self.tanh = nn.Tanh()
 
     def forward(self, noise, features):
         # noise has dimension 2048 * 7 * 7 (TODO: check adding layer here)
         x = self.gen_block1(noise, features[3])
         x = self.gen_block2(x, features[2])
         x = self.gen_block3(x, features[1])
+        x, _ = self.attn1(x)
         x = self.gen_block4(x, features[0])
         x = self.last_conv_block(x)
+        x = self.tanh(x)
         return x
--- train_model.py
+++ train_model.py
@@ -9,6 +9,7 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torch.nn as nn
 import argparse
+import time
 
 from classifier import Classifier
 from generator import Generator
@@ -40,7 +41,7 @@ def _main():
                                      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                                  ]))
     print('set data loader')
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=2)
 
     # classifier = torch.load(r"C:\Users\tomer\OneDrive\Desktop\sadna\pyramid_project\classifier")
     classifier = torch.load("./classifier")
@@ -60,17 +61,25 @@ def _main():
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
     noise = torch.zeros(args.batch_size, 2048, 7, 7, device=torch.device('cuda:0'))
-
+    starting_time = time.time()
     print("Starting Training Loop...")
     for epoch in range(num_of_epochs):
         print('epoch:', epoch)
         train_loss = 0.0  # monitor training loss
+        batch_count = 0
         for data in train_loader:
+            batch_count += 1
+            if batch_count % 10 == 1:
+                print('batch', batch_count, 'start, time =', time.time() - starting_time, 'seconds')
+                starting_time = time.time()
             images, _ = data
             images = images.cuda()  # change to gpu tensor
             optimizer.zero_grad()  # zeros previous grads
             _, features = classifier(images)
             features = features[1:5] # for now working with res blocks only
+            if images.shape[0] != noise.shape[0]:
+                del noise
+                noise = torch.zeros(images.shape[0], 2048, 7, 7, device=torch.device('cuda:0'))
             outputs_images = generator(noise, features)  # forward pass
             outputs_images = normalizer(outputs_images)
             _, outputs_images_features = classifier(outputs_images)
@@ -81,12 +90,21 @@ def _main():
             loss.backward()  # back prop
             optimizer.step()  # modify weights
             train_loss += loss.item() * images.size(0)
+            del data
+            del features
+            del images
+            del outputs_images
+            del outputs_images_features
+            del loss
         # print avg training statistics
         train_loss = train_loss / len(train_loader)
         print('Epoch: {}/{} \tTraining Loss: {:.6f}'.format(epoch + 1, num_of_epochs, train_loss))
         # save the model, if needed
         if (not args.only_final and epoch % 10 == 0) or epoch == num_of_epochs - 1:
             torch.save(generator.state_dict(), './' + args.model_name + '/' + args.model_name + '_' + str(epoch + 1))
+        elif epoch == 3 or epoch == 5:
+            torch.save(generator.state_dict(), './' + args.model_name + '/' + args.model_name + '_' + str(epoch + 1))
+
 
     # test_root = args.test_path
     # test_set = dset.ImageFolder(root=test_root,
@@ -101,10 +119,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser('choose hyperparameters')
     parser.add_argument('--batch-size', default=16, type=int)
     parser.add_argument('--lr', default=0.001, type=float)
-    parser.add_argument('--epochs', default=10, type=int)
+    parser.add_argument('--epochs', default=20, type=int)
     parser.add_argument('--only-final', action='store_true')
     parser.add_argument('--model-name')
-    parser.add_argument('--train-path', default='/home/dcor/datasets/smallPlaces')
+    parser.add_argument('--train-path', default='/home/dcor/datasets/places365')
     parser.add_argument('--test-path', default='../testImages')
     args = parser.parse_args()
     print(args)
