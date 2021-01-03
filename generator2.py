bb0b26d038bba958bab65e6d245c2be4dfd370c6
tomer1727 <tomer1727@gmail.com>
2021-01-03 14:23:56 +0200

    expd

---
+++ generator2.py
@@ -0,0 +1,166 @@
+import torch
+import torch.nn as nn
+
+
+def init_weights(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.2)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.2)
+        nn.init.constant_(m.bias.data, 0)
+    elif classname.find('Linear') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.2)
+
+
+class ConvTransposeBlock(nn.Module):
+    """
+    Basic block of our model, includes Sequential model of conv layer -> batch normalization -> Relu activation
+    This block keeps the original dimension
+    """
+    def __init__(self, channels, num_filters, filter_size, activation='relu', stride=1, padding=1):
+        super(ConvTransposeBlock, self).__init__()
+        self.block = nn.Sequential(nn.utils.spectral_norm(nn.ConvTranspose2d(channels, num_filters, kernel_size=filter_size, padding=padding, stride=stride)),
+                                   nn.BatchNorm2d(num_filters),
+                                   nn.ReLU(inplace=True) if activation == 'relu' else nn.Identity())
+
+    def forward(self, inputs):
+        outputs = self.block(inputs)
+        return outputs
+
+    def init_weights(self):
+        self.block.apply(init_weights)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.in_channels, self.out_channels = in_channels, out_channels
+        self.conv_block1 = ConvTransposeBlock(in_channels, in_channels, 1, padding=0)
+        self.conv_block2 = ConvTransposeBlock(in_channels, in_channels, 4, stride=2)
+        self.conv_block3 = ConvTransposeBlock(in_channels, out_channels, 1, padding=0, activation='none')
+        self.skip_block = nn.Sequential(nn.utils.spectral_norm(nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=2, stride=2, padding=0, bias=False)),
+                                        nn.BatchNorm2d(out_channels))
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        residual = x
+        if self.in_channels != self.out_channels:
+            residual = self.skip_block(residual)
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
+        x += residual
+        x = self.activation(x)
+        return x
+
+
+class SelfAttentionLayer(nn.Module):
+    """ Self attention Layer"""
+
+    def __init__(self, in_dim):
+        super(SelfAttentionLayer, self).__init__()
+        self.chanel_in = in_dim
+
+        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax = nn.Softmax(dim=-1)  #
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps( B X C X W X H)
+            returns :
+                out : self attention value + input feature
+                attention: B X N X N (N is Width*Height)
+        """
+        m_batchsize, C, width, height = x.shape
+        proj_query = self.query_conv(x).view(m_batchsize, -1, width * height).permute(0, 2, 1)  # B X CX(N)
+        proj_key = self.key_conv(x).view(m_batchsize, -1, width * height)  # B X C x (*W*H)
+        energy = torch.bmm(proj_query, proj_key)  # transpose check
+        attention = self.softmax(energy)  # BX (N) X (N)
+        proj_value = self.value_conv(x).view(m_batchsize, -1, width * height)  # B X C X N
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, width, height)
+
+        out = self.gamma * out + x
+        return out, attention
+
+    def init_weights(self):
+        self.query_conv.apply(init_weights)
+        self.key_conv.apply(init_weights)
+        self.value_conv.apply(init_weights)
+
+
+class GeneratorBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv_block1 = ConvTransposeBlock(in_channels, out_channels, 4, stride=2, padding=1)
+        # self.conv_block2 = ConvTransposeBlock(out_channels, out_channels, 3, stride=1, padding=1)
+        self.features_conv_block = ConvTransposeBlock(in_channels, out_channels, 4, stride=2, padding=1)
+
+    def forward(self, gen, features):
+        gen = self.conv_block1(gen)
+        # gen = self.conv_block2(gen)
+        features = self.features_conv_block(features)
+        return gen + features
+
+    def init_weights(self):
+        self.conv_block1.init_weights()
+        # self.conv_block2.init_weights()
+        self.features_conv_block.init_weights()
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv_block1 = ConvTransposeBlock(256, 256, 4, stride=1, padding=0)
+        self.conv_block2 = ConvTransposeBlock(256, 512, 3, stride=2, padding=1)
+        self.gen_block1 = GeneratorBlock(512, 256)
+        self.gen_block2 = GeneratorBlock(256, 128)
+        self.gen_block3 = GeneratorBlock(128, 64)
+        # self.attn1 = SelfAttentionLayer(in_dim=64)
+        self.gen_block4 = GeneratorBlock(64, 64)
+        # self.attn2 = SelfAttentionLayer(in_dim=64)
+        self.gen_block5 = GeneratorBlock(64, 64)
+        self.last_conv = nn.ConvTranspose2d(64, 3, kernel_size=3, stride=1, padding=1)
+        self.tanh = nn.Tanh()
+
+        # self.conv_block1 = ConvTransposeBlock(3, 3, 3, stride=1, padding=1)
+        # self.conv2 = nn.ConvTranspose2d(3, 3, kernel_size=3, padding=1, stride=1)
+        # self.conv_block2 = ConvTransposeBlock(3, 3, 3, stride=1, padding=1)
+
+    def forward(self, noise, features):
+        # noise has dimension 256 * 1 * 1
+        x = self.conv_block1(noise)
+        x = self.conv_block2(x)
+        x = self.gen_block1(x, features[4])
+        x = self.gen_block2(x, features[3])
+        x = self.gen_block3(x, features[2])
+        # x, _ = self.attn1(x)
+        x = self.gen_block4(x, features[1])
+        # x, _ = self.attn2(x)
+        x = self.gen_block5(x, features[0])
+        x = self.last_conv(x)
+        # x = self.conv_block1(x)
+        # x = self.conv2(x)
+        x = self.tanh(x)
+        return x
+
+    def init_weights(self):
+        self.conv_block1.init_weights()
+        self.conv_block2.init_weights()
+        self.gen_block1.init_weights()
+        self.gen_block2.init_weights()
+        self.gen_block3.init_weights()
+        self.gen_block4.init_weights()
+        self.gen_block5.init_weights()
+        # self.attn1.init_weights()
+        # self.attn2.init_weights()
+        self.last_conv.apply(init_weights)
+
+
---
+++ train_model2.py
@@ -0,0 +1,227 @@
+import torch
+from torch.autograd import Variable
+from PIL import Image
+import os
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torch.nn as nn
+import argparse
+import time
+import random
+
+from classifier import Classifier
+from generator2 import Generator
+from discriminator2 import Discriminator
+
+
+real_label_D = 0.9
+real_label_G = 1.0
+fake_label = 0.1
+
+
+def print_gpu_details():
+    """
+    print gpu details to make sure gpu available
+    """
+    print('Is GPU available:', torch.cuda.is_available())
+    if torch.cuda.is_available():
+        print('Num of GPUs:', torch.cuda.device_count())
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.2)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.2)
+        nn.init.constant_(m.bias.data, 0)
+    elif classname.find('Linear') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.2)
+
+
+def _main():
+    print_gpu_details()
+    device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
+    train_root = args.train_path
+    # train_root = r"C:\Users\tomer\OneDrive\Documents\datasets\model2\cars\train"
+
+    image_size = 256
+    cropped_image_size = 224
+    print("set image folder")
+    train_set = dset.ImageFolder(root=train_root,
+                                 transform=transforms.Compose([
+                                     transforms.Resize(image_size),
+                                     transforms.CenterCrop(cropped_image_size),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                                 ]))
+    print('set data loader')
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=4, pin_memory=True)
+
+    # classifier = torch.load(r"C:\Users\tomer\OneDrive\Desktop\sadna\pyramid_project\classifier")
+    classifier = torch.load("./classifier18")
+    classifier.eval()
+    generator = Generator()
+    # generator.load_state_dict(torch.load('./exp/expG_400'))
+    discriminator = Discriminator()
+    if torch.cuda.device_count() > 1:
+        classifier = nn.DataParallel(classifier)
+        generator = nn.DataParallel(generator)
+        discriminator = nn.DataParallel(discriminator)
+    classifier.to(device)
+    generator.to(device)
+    discriminator.to(device)
+
+    # weights init
+    generator.init_weights()
+    discriminator.init_weights()
+
+    # losses + optimizers
+    criterion_features = nn.L1Loss()
+    criterion_bce_g = nn.BCELoss()
+    criterion_bce_d = nn.BCELoss()
+    optimizer_generator = optim.Adam(generator.parameters(), lr=args.lr, betas=(0.5, 0.999))
+    optimizer_discriminator = optim.Adam(discriminator.parameters(), lr=4*args.lr, betas=(0.5, 0.999))
+
+    num_of_epochs = args.epochs
+
+    normalizer = transforms.Compose([
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    # noise = torch.zeros(args.batch_size, 512, 1, 1, device=torch.device('cuda:0'))
+    starting_time = time.time()
+    iterations = 0
+    print("Starting Training Loop...")
+    for epoch in range(num_of_epochs):
+        for data in train_loader:
+            iterations += 1
+            if iterations % 100 == 1:
+                print('epoch:', epoch, ', iter', iterations, 'start, time =', time.time() - starting_time, 'seconds')
+                starting_time = time.time()
+            images, _ = data
+            images = images.to(device)  # change to gpu tensor
+            # generator update
+            # optimizer_generator.zero_grad()  # zeros previous grads
+            generator.zero_grad()
+            _, features = classifier(images)
+            # if images.shape[0] != noise.shape[0]:
+            #     del noise
+            # (torch.randn(images.shape[0], 3, 224, 224, device=device)) * std
+            noise = torch.randn(images.shape[0], 256, 1, 1, device=device)
+            fake_images = generator(noise, features)
+            fake_images = 0.5 * (fake_images + 1)
+            fake_images = normalizer(fake_images)
+
+            # if iterations % 4 != 2:
+            # print('Generator update')
+            label = torch.full((images.shape[0],), real_label_G, dtype=torch.float, device=device)
+            discriminator_preds = discriminator(fake_images).view(-1)
+            loss_adversarial = criterion_bce_g(discriminator_preds, label)
+            # loss_adversarial.backward()
+            total_loss = loss_adversarial
+            # optimizer_generator.step()  # modify weights
+            if iterations % 4 == 2:
+                _, outputs_images_features = classifier(fake_images)
+                loss_features = criterion_features(features[0], outputs_images_features[0])
+                for i in range(1, len(features) - 1):
+                    loss_features += criterion_features(features[i], outputs_images_features[i])  # calculate loss
+                # loss_features += criterion_features(images, fake_images)
+                # loss_features.backward()
+                if iterations % 40 == 2:
+                    print('features loss: {:.6f}'.format(loss_features.item()))
+                total_loss += loss_features
+                del outputs_images_features
+
+            total_loss.backward()
+            optimizer_generator.step()  # modify weights
+
+            if iterations % 3 == 1:
+                # discriminator update
+                # print('Discriminator update')
+                # optimizer_discriminator.zero_grad()
+                discriminator.zero_grad()
+                # real images batch
+                label = torch.full((images.shape[0],), real_label_D, dtype=torch.float, device=device)
+                label += (torch.randn(images.shape[0], device=device)) * 0.04
+                flip = torch.rand(1)[0] > 0.75
+                flip = flip.item()
+                if flip:
+                    label[random.randint(0, args.batch_size - 1)] = fake_label
+                std = 0.1 / (iterations // 1000 + 1)
+                white_noise = (torch.randn(images.shape[0], 3, 224, 224, device=device)) * std
+                images = images + white_noise
+                output = discriminator(images).view(-1) # forward pass
+                acc = torch.sum(output > 0.5)
+                loss_real = criterion_bce_d(output, label)
+                loss_real.backward()
+                # fake batch
+                label.fill_(fake_label)
+                label += (torch.randn(images.shape[0], device=device)) * 0.04
+                label = torch.max(torch.zeros(images.shape[0], device=device), label)
+                if flip:
+                    label[random.randint(0, args.batch_size - 1)] = real_label_D
+                fake_images = fake_images + white_noise
+                output = discriminator(fake_images.detach()).view(-1) # forward pass
+                acc += torch.sum(output < 0.5)
+                acc_np = acc.cpu().numpy()
+                loss_fake = criterion_bce_d(output, label)
+                loss_fake.backward()
+                discriminator_loss = loss_real + loss_fake
+                # discriminator_loss.backward()
+                optimizer_discriminator.step()
+                print('discriminator accuracy:', acc_np, '/', args.batch_size * 2)
+                del white_noise
+                del loss_fake
+                del loss_real
+                del acc
+
+            if iterations % 30 == 1:
+                # print('iter: {} \tfeatures Loss: {:.6f}, discriminator loss: {:.6f}, generator loss: {:.6f}'.format(
+                #     batch_count, loss_features.item(), discriminator_loss.item(), loss_adversarial.item()))
+                print('iter: {} \t, discriminator loss: {:.6f}, generator loss: {:.6f}'.format(iterations, discriminator_loss.item(), loss_adversarial.item()))
+                # print('iter: {} \tfeatures Loss: {:.6f}'.format(batch_count, loss_features.item()))
+                del loss_adversarial
+            del data
+            del features
+            del images
+            del fake_images
+            del noise
+            if iterations % 3 == 1:
+                del discriminator_loss
+
+            if iterations % 500 == 1:
+                torch.save(generator.state_dict(), './' + args.model_name + '/' + args.model_name + 'G')
+                torch.save(discriminator.state_dict(), './' + args.model_name + '/' + args.model_name + 'D')
+
+        # print('Epoch: {}/{} \tTraining Loss: {:.6f}'.format(epoch + 1, num_of_epochs, train_loss))
+        # save the model, if needed
+        if (not args.only_final and epoch % 10 == 0) or epoch == num_of_epochs - 1:
+            torch.save(generator.state_dict(), './' + args.model_name + '/' + args.model_name + 'G_' + str(epoch + 1))
+            torch.save(discriminator.state_dict(), './' + args.model_name + '/' + args.model_name + 'D_' + str(epoch + 1))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('choose hyperparameters')
+    parser.add_argument('--batch-size', default=16, type=int)
+    parser.add_argument('--lr', default=0.0001, type=float)
+    parser.add_argument('--epochs', default=20, type=int)
+    parser.add_argument('--only-final', action='store_true')
+    parser.add_argument('--model-name')
+    parser.add_argument('--train-path', default='/home/dcor/datasets/places365')
+    parser.add_argument('--test-path', default='../testImages')
+    args = parser.parse_args()
+    print(args)
+    if args.model_name is None:
+        print("Must specify model name")
+        exit(1)
+    if not os.path.exists('./' + args.model_name):
+        os.mkdir('./' + args.model_name)
+    arg_dic = vars(args)
+    with open('./' + args.model_name + '/hyperparams.txt', 'w') as file:
+        for key in arg_dic:
+            file.write(key + ': {}\n'.format(arg_dic[key]))
+
+    _main()
